04741e848e5a3cfd68baa3c5d8a3271124ccadb3
marciocarolino <marciocarolino@gmail.com>
2021-02-03 03:51:01 -0300

    Feito os links para que possa assistir os trailer

---
+++ js/filmes.js
@@ -0,0 +1,47 @@
+function mulherMaravilha() {
+    window.open('https://www.youtube.com/watch?v=8XrFAXykgoc&t=9s&ab_channel=Ingresso.com');
+}
+
+function ava() {
+    window.open('https://www.youtube.com/watch?v=Mot5ovqTPXo&ab_channel=TrailersBR');
+}
+
+function godzilla() {
+    window.open('https://www.youtube.com/watch?v=4vhxYqsqZb0&ab_channel=Cinemaginando');
+}
+
+function O_Recife_Da_Dory() {
+    window.open('https://www.youtube.com/watch?v=S2AVzedy53A&ab_channel=FilmIsNowMovieTrailersInternational');
+}
+
+function Resgate() {
+    window.open('https://www.youtube.com/watch?v=_IANQ2pNzaQ&ab_channel=TrailersemPortugu%C3%AAs')
+}
+
+function Vikings() {
+    window.open('https://www.youtube.com/watch?v=9GgxinPwAGc&ab_channel=RewatchAgain');
+}
+
+function lupin() {
+    window.open('https://www.youtube.com/watch?v=WpSweeyuHb8&ab_channel=NetflixPortugal');
+}
+
+function cobra_kai() {
+    window.open('https://www.youtube.com/watch?v=xCwwxNbtK6Y&ab_channel=CobraKai');
+}
+
+function coring() {
+    window.open('https://www.youtube.com/watch?v=621pfj0EfIc&ab_channel=WarnerBros.PicturesBrasil');
+}
+
+function kong() {
+    window.open('https://www.youtube.com/watch?v=VU-m5T6wBzY&ab_channel=WarnerBros.PicturesBrasil');
+}
+
+function lucifer() {
+    window.open('https://www.youtube.com/watch?v=X4bF_quwNtw&ab_channel=Lucifer');
+}
+
+function expresso() {
+    window.open('https://www.youtube.com/watch?v=zstihByJe9I&ab_channel=NetflixBrasil');
+}
